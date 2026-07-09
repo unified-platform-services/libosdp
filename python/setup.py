88fb0b1464d5746e6af1c886a178b4405e1e0072
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2026-07-09 19:47:16 +0200

    Release v3.2.3
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -12,7 +12,7 @@ import shutil
 import subprocess
 
 project_name = "libosdp"
-project_version = "3.2.2"
+project_version = "3.2.3"
 current_dir = os.path.dirname(os.path.realpath(__file__))
 repo_root = os.path.realpath(os.path.join(current_dir, ".."))
 
